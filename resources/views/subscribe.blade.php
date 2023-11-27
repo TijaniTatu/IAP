d75a9646af051ee16f3e1a50cab913773d48f26b
LancemDev <munyaolance1@gmail.com>
2023-11-27 15:28:23 +0300

    :)

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- ... -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>API Web</title>
+  @vite(['resources/css/app/css', 'resources/js/app.js'])
 </head>
+
 <body class="font-poppins antialiased">
     <div id="view" class="h-full w-screen flex flex-row" x-data="{ sidenav: true }">
         <!-- ... -->
